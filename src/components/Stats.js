c226755a290750a4d3619583fb72e5d29ab917af
Kyle <ktate2@ggc.edu>
2021-01-19 14:05:22 -0500

    React pokedex proj using pokeAPI finished except for responsive UI

---
+++ src/components/Stats.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react';
+
+
+const Stats = (props) => {
+  
+
+   console.log("Data for pocketmosterrrs" + props);
+  return (
+    <div className="Stats">
+      
+    {props.pokemonData.map((data) => {
+      return(
+        <div className="container">
+
+       <div className="stats-table">
+       <h1 className="stats-title">Stats</h1>
+       <tr>
+                <th className="table-cell-stats">{data.stats[0].stat.name}</th>   
+                <th className="table-cell-stats">{data.stats[1].stat.name}</th>   
+                <th className="table-cell-stats">{data.stats[2].stat.name}</th>         
+                <th className="table-cell-stats">{data.stats[3].stat.name}</th>
+                <th className="table-cell-stats">{data.stats[4].stat.name}</th>
+                <th className="table-cell-stats">{data.stats[5].stat.name}</th>
+        </tr>
+
+        <tr>
+                <td className="table-cell-stats">{data.stats[0].base_stat}</td>
+                <td className="table-cell-stats">{data.stats[1].base_stat}</td>
+                <td className="table-cell-stats">{data.stats[2].base_stat}</td>
+                <td className="table-cell-stats">{data.stats[3].base_stat}</td>
+                <td className="table-cell-stats">{data.stats[4].base_stat}</td>
+                <td className="table-cell-stats">{data.stats[5].base_stat}</td>     
+        </tr>
+        </div>
+      </div>
+        
+      )
+
+    })}
+
+    </div>
+  );
+
+
+
+    
+};
+
+
+export default Stats;
